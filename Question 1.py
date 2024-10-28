755a0a7fab1703efffcce7f44c85984d08b92850
eddiesmarter <edwinwachirah@gmail.com>
2024-10-28 08:59:41 +0300

    question 1

---
+++ Question 1.py
@@ -0,0 +1,10 @@
+# Variable Assignment & Arithmetic Operations
+# Assign numbers to variables a and b
+a = 50
+b = 5
+
+# Perform operations and print results
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
---
+++ Question 2.py
@@ -0,0 +1,10 @@
+#Program  for Odd or Even Checker
+
+# Ask the user for a number
+number = int(input("Enter a number: "))
+
+# Check if the number is odd or even
+if number % 2 == 0:
+        print(f"The number {number} is even.")
+else:
+        print(f"The number {number} is odd.")
